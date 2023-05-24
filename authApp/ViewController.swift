9927eff0563e4dc334ccb77ccfe28e156b39ced5
Askhat Gumirov <agumirov.dev@gmail.com>
2023-05-24 19:15:30 +0600

    Initial Commit

---
+++ authApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  authApp
+//
+//  Created by G G on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+}
+
